792c34d687208b6554452a92e951167d7e39c1b8
Yaswanth <rajiyash317@gmail.com>
2025-07-15 14:07:28 +0530

    Initial commit for project + team entity creation

---
+++ src/main/java/com/example/accessManager/entity/Team.java
@@ -0,0 +1,25 @@
+package com.example.accessManager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "team")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "is_active", columnDefinition = "BOOLEAN DEFAULT false")
+    private Boolean isActive;
+}
---
+++ src/test/java/com/example/accessManager/AccessManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
